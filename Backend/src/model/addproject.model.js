471fb42137549b40aedb4de3363669c38694e201
tejas <tejasnasre120@gmail.com>
2024-03-15 12:57:43 +0530

    Make A User Model And Add Project Model

---
+++ Backend/src/model/addproject.model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ProjectSchema = new Schema(
+  {
+    image: {
+      type: String, //Cloudinary image URL
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    skillsRequired: {
+      type: String,
+      required: true,
+    },
+    createdBy: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    liveLink: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Project = mongoose.model("Project", ProjectSchema);
+
+module.exports = Project;
