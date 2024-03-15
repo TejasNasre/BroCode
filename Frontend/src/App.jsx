279a9b3bf70f3dcb4c43becfe456e76e07642dfb
vrushabh-mowade <vrushabhmowade@gmail.com>
2024-03-15 13:27:44 +0530

    footer and v

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar.jsx";
 import { Outlet } from "react-router-dom";
+// import {Signup} from "./pages/Signup.jsx"
+import Footer from "./components/Footer.jsx"
 
 export function App() {
   return (
     <>
     <Navbar/>
     <Outlet/>
-    {/* <Footer /> */}
+    <Footer />
     </>
     )
 }
