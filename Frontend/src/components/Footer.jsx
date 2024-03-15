93ec6121c5a976532d18d043044bf5750e9cdec3
vrushabh-mowade <vrushabhmowade@gmail.com>
2024-03-15 14:29:31 +0530

    commit4

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,13 +1,14 @@
 
 export default function Footer(){
+    return<>
 
-    <footer className="bg-white dark:bg-gray-900">
+<footer className="bg-white dark:bg-gray-900">
     <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
                 <a href="https://flowbite.com/" className="flex items-center">
                     <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 me-3" alt="FlowBite Logo" />
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Skill Swap</span>
                 </a>
             </div>
             <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
@@ -85,6 +86,8 @@ export default function Footer(){
         </div>
     </div>
 </footer>
+    </>
+
 
 
 }
